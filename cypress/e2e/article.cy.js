c8bb301a8a7141bfc7321988ce52b1a20eade8dc
MaksSavchenko <savchenkomaks96@gmail.com>
2025-05-12 12:36:45 +0300

    Add article flow tests

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,46 @@
-describe('', () => {
-  before(() => {
+import { faker } from '@faker-js/faker';
 
+describe('Article flow', () => {
+  const title = faker.lorem.words(3);
+  const description = faker.lorem.sentence(3);
+  const article = faker.lorem.sentence(20);
+  const tag = faker.lorem.word();
+
+  beforeEach(() => {
+    cy.visit('/');
+
+    cy.task('generateUser').then((data) => {
+      const { email, username, password } = data;
+      cy.login(email, username, password);
+      cy.reload();
+    });
   });
 
-  it('', () => {
+  it('User should be able to create an article', () => {
+    cy.contains('.nav-link', 'New Article').click();
 
+    cy.getPlaceholder('Article Title').type(title);
+    cy.getPlaceholder('What\'s this article about?').type(description);
+    cy.getPlaceholder('Write your article (in markdown)').type(article);
+    cy.getPlaceholder('Enter tags').type(tag +'{enter}');
+
+    cy.get('.btn').click();
+
+    cy.contains('.nav-link', 'Home').click();
+    cy.contains('.nav-link', 'Global Feed').click();
+    cy.get('.article-preview').should('contain.text', title);
   });
 
-  it('', () => {
 
+  it('User should be able to delete the article', () => {
+    cy.createArticle(title, description, article);
+
+    cy.contains('.nav-link', 'Global Feed').click();
+    cy.get('.article-preview').contains('h1', `Article title: ${title}`).click();
+
+    cy.contains('button', 'Delete Article').type('{enter}');
+
+    cy.contains('.nav-link', 'Global Feed').click();
+    cy.get('h1').should('not.have.value', `Article title: ${title}`);
   });
 });
